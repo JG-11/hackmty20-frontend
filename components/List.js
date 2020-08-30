2da592f5fd25e9877c595bd3875f182b67ebdb16
Genaro Almaraz <jg08950@gmail.com>
2020-08-29 19:55:34 -0500

    List: SFC

--- components/List.js
+++ components/List.js
@@ -1,42 +1,40 @@
 import { Table } from 'react-bootstrap'
 
+const List = props => {
+    let i = 0
 
-class List extends React.Component {
-    render() {
-        let i = 0
-        
-        return (
-            <Table responsive>
-                <thead>
-                    <tr>
-                        <th>
+    return (
+        <Table responsive>
+            <thead>
+                <tr>
+                    <th>
                         #
-                        </th>
-                        <th>
+                    </th>
+                    <th>
                         Username
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                {
-                    this.props.data.map(name => {
-                    i++
-                    
-                    return (
-                        <tr key={i}>
-                            <td>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                props.data.map(name => {
+                
+                i++
+                
+                return (
+                    <tr key={i}>
+                        <td>
                             { i }
-                            </td>
-                            <td>
+                        </td>
+                        <td>
                             { name }
-                            </td>
-                        </tr>
-                    )})
-                }
-                </tbody>
-            </Table>
-        )
-    }
+                        </td>
+                    </tr>
+                )})
+            }
+            </tbody>
+        </Table>
+    )
 }
 
 export default List
